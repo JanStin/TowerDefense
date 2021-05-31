cd2ae729bc616d0d0fdae35b8e48627bc542719c
Mixail Kirilin <mixail.kirilin2012@yandex.ru>
2021-05-31 22:45:40 +0300

    Setting the current level tower

--- Assets/Scripts/MonsterData.cs
+++ Assets/Scripts/MonsterData.cs
@@ -6,4 +6,41 @@ using UnityEngine;
 public class MonsterData : MonoBehaviour
 {
     public List<MonsterLevel> levels;
+
+    private MonsterLevel _currentLevel;
+
+    public MonsterLevel CurrentLevel
+    {
+        get
+        {
+            return _currentLevel;
+        }
+
+        set
+        {
+            _currentLevel = value;
+            int currentLevelIndex = levels.IndexOf(_currentLevel);
+
+            GameObject levelVisualization = levels[currentLevelIndex].visualization;
+            for (int i = 0; i < levels.Count; i++)
+            {
+                if (levelVisualization != null)
+                {
+                    if (i == currentLevelIndex)
+                    {
+                        levels[i].visualization.SetActive(true);
+                    }
+                    else
+                    {
+                        levels[i].visualization.SetActive(false);
+                    }
+                }
+            }
+        }
+    }
+
+    private void OnEnable()
+    {
+        CurrentLevel = levels[0];
+    }
 }
