feb0c3d604536cfb9b25ce4ccfe4bc3a955e288d
Mixail Kirilin <mixail.kirilin2012@yandex.ru>
2021-05-31 22:29:58 +0300

    Added spots. Added beginer scripts for tower.

---
+++ Assets/Scripts/MonsterLevel.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[System.Serializable]
+public class MonsterLevel
+{ 
+    public int cost;
+    public GameObject visualization;
+}
---
+++ Assets/Scripts/PlaceMonster.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceMonster : MonoBehaviour
+{
+    public GameObject monsterPrefab;
+    private GameObject monster;
+
+    private bool CanPlaceMonster()
+    {
+        return monster == null;
+    }
+
+    private void OnMouseUp()
+    {
+        if (CanPlaceMonster())
+        {
+            monster = (GameObject)Instantiate(monsterPrefab, transform.position, Quaternion.identity);
+
+            AudioSource audioSource = gameObject.GetComponent<AudioSource>();
+            audioSource.PlayOneShot(audioSource.clip);
+
+            // TODO: вычитать золото
+        }
+    }
+}
