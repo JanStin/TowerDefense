8c75cbb53f6c8e6306eadce1bbf857128ed2713b
Mixail Kirilin <mixail.kirilin2012@yandex.ru>
2021-06-01 22:37:11 +0300

    feat: upgrade tower

--- Assets/Scripts/MonsterData.cs
+++ Assets/Scripts/MonsterData.cs
@@ -7,19 +7,19 @@ public class MonsterData : MonoBehaviour
 {
     public List<MonsterLevel> levels;
 
-    private MonsterLevel _currentLevel;
+    private MonsterLevel currentLevel;
 
     public MonsterLevel CurrentLevel
     {
         get
         {
-            return _currentLevel;
+            return currentLevel;
         }
 
         set
         {
-            _currentLevel = value;
-            int currentLevelIndex = levels.IndexOf(_currentLevel);
+            currentLevel = value;
+            int currentLevelIndex = levels.IndexOf(currentLevel);
 
             GameObject levelVisualization = levels[currentLevelIndex].visualization;
             for (int i = 0; i < levels.Count; i++)
@@ -42,5 +42,30 @@ public class MonsterData : MonoBehaviour
     private void OnEnable()
     {
         CurrentLevel = levels[0];
+    }
+
+    public MonsterLevel GetNextLevel()
+    {
+        int currentLevelIndex = levels.IndexOf(currentLevel);
+        int maxLevelIndex = levels.Count - 1;
+
+        if (currentLevelIndex < maxLevelIndex)
+        {
+            return levels[currentLevelIndex + 1];
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public void IncreaseLevel()
+    {
+        int currentLevelIndex = levels.IndexOf(currentLevel);
+
+        if (currentLevelIndex < levels.Count - 1)
+        {
+            CurrentLevel = levels[currentLevelIndex + 1];
+        }
     }
 }
