5e47506f5e6febc8b329a72dafc4041c4402fbeb
Mixail Kirilin <mixail.kirilin2012@yandex.ru>
2021-06-06 12:45:47 +0300

    feat: waves enemies

--- Assets/Scripts/SpawnEnemy.cs
+++ Assets/Scripts/SpawnEnemy.cs
@@ -6,10 +6,52 @@ public class SpawnEnemy : MonoBehaviour
 {
     public GameObject[] waypoints;
 
-    public GameObject testEnemyPrefab;
+    public Wave[] waves;
+    public int timeBetweenWaves = 5;
+
+    private GameManagerBehavior gameManager;
+
+    private float lastSpawnTime;
+    private int enemiesSpawned = 0;
 
     private void Start()
     {
-        Instantiate(testEnemyPrefab).GetComponent<MoveEnemy>().waypoints = waypoints;
+        lastSpawnTime = Time.time;
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManagerBehavior>();
+    }
+
+    private void Update()
+    {
+        int currentWave = gameManager.Wave;
+        if (currentWave < waves.Length)
+        {
+            float timeInterval = Time.time - lastSpawnTime;
+            float spawnInterval = waves[currentWave].spawnInterval;
+
+            if (((enemiesSpawned == 0 && timeInterval > timeBetweenWaves) || timeInterval > spawnInterval)
+                && enemiesSpawned < waves[currentWave].maxEnemies)
+            {
+                lastSpawnTime = Time.time;
+
+                GameObject newEnemy = (GameObject)Instantiate(waves[currentWave].enemyPrefab);
+                newEnemy.GetComponent<MoveEnemy>().waypoints = waypoints;
+                enemiesSpawned++;
+            }
+
+            if (enemiesSpawned == waves[currentWave].maxEnemies 
+                && GameObject.FindGameObjectWithTag("Enemy") == null)
+            {
+                gameManager.Wave++;
+                gameManager.Gold = Mathf.RoundToInt(gameManager.Gold * 1.1f);
+                enemiesSpawned = 0;
+                lastSpawnTime = Time.time;
+            }
+        }
+        else
+        {
+            gameManager.gameOver = true;
+            GameObject gameOverText = GameObject.FindGameObjectWithTag("GameWon");
+            gameOverText.GetComponent<Animator>().SetBool("gameOver", true);
+        }
     }
 }
---
+++ Assets/Scripts/Wave.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[System.Serializable]
+public class Wave
+{
+    public GameObject enemyPrefab;
+    public float spawnInterval = 2;
+    public int maxEnemies = 20;
+}
